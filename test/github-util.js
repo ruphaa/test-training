141857ff47af6375a9fa7b90fe0a1a8321a1146a
Ruphaa Ganesan <ruphaag@INLM-1275.local>
2018-01-27 22:42:11 +0530

    Removed log statement

--- test/github-util.js
+++ test/github-util.js
@@ -2,11 +2,10 @@ import { expect } from 'chai';
 import { getReadMeContents } from '../src/github-util';
 
 describe.only('github util', () => {
-  it.only('Get Readme Contents - should return readme data', (done) => {
+  it.only('get Readme Contents - should return json', (done) => {
     const owner = 'k88hudson';
     const repo = 'git-flight-rules';
     getReadMeContents(owner, repo).then((res) => {
-      console.log(res.status);
       expect(res.status).to.equal(200);
       done();
     });
