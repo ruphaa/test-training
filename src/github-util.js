7e88f43a4a48d97cbfacdb0f6fc223d1d9966927
Ruphaa Ganesan <ruphaag@INLM-1275.local>
2018-01-28 15:44:00 +0530

    Few methods are included in github-util

--- src/github-util.js
+++ src/github-util.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
 
-const getReadMeContents = (owner, repo) => {
+const getReadMeFile = (owner, repo) => {
   const baseURL = 'https://api.github.com/repos';
   const requestUrl = `${baseURL}/${owner}/${repo}/readme`;
 
   return axios.get(requestUrl)
-    .then(response => response)
-    .catch((err) => {
-      // TODO - handle error scenario
-      console.log(err); // eslint-disable-line
-    });
+    .then(res => res)
+    .catch(err => null); //eslint-disable-line
 };
+
+const decodeBase64String = (base64String) => {
+  if (typeof base64String !== 'string') return null;
+  const decodedString = Buffer.from(base64String, 'base64');
+  return decodedString.toString();
+};
+
+const getReadMeData = (owner, repo) =>
+  getReadMeFile(owner, repo)
+    .then((res) => {
+      if (res && res.data && res.data.content) decodeBase64String(res.data.content);
+      return null;
+    });
 
 export {
   // eslint-disable-next-line
-  getReadMeContents
+  getReadMeFile,
+  decodeBase64String,
+  getReadMeData
 };
--- test/github-util.js
+++ test/github-util.js
@@ -1,13 +1,45 @@
 import { expect } from 'chai';
-import { getReadMeContents } from '../src/github-util';
+import { getReadMeFile, decodeBase64String, getReadMeData } from '../src/github-util';
 
 describe.only('github util', () => {
-  it.only('get Readme Contents - should return json', (done) => {
+  it('get Readme Contents - should return json', (done) => {
     const owner = 'k88hudson';
     const repo = 'git-flight-rules';
-    getReadMeContents(owner, repo).then((res) => {
+    getReadMeFile(owner, repo).then((res) => {
       expect(res.status).to.equal(200);
       done();
     });
-  }).timeout(5000);
-});
+  });
+
+  it('decode the base64 string', () => {
+    const input = 'SSBsb3ZlIFRERCA8Mw==';
+    const output = decodeBase64String(input);
+    expect(output).to.equal('I love TDD <3');
+  });
+
+  it('monkey test - base64 decode', () => {
+    let input = 22;
+    let output = decodeBase64String(input);
+    expect(output).to.be.null;
+    input = null;
+    output = decodeBase64String(input);
+    expect(output).to.be.null;
+  });
+
+  it('get readme data', () => {
+    const owner = 'k88hudson';
+    const repo = 'git-flight-rules';
+    getReadMeData(owner, repo).then((data) => {
+    console.log(data); // eslint-disable-line
+    });
+  });
+
+  it('should return null when something goes wrong', (done) => {
+    const owner = 'k88hudcson';
+    const repo = 'git-flight-rules';
+    getReadMeData(owner, repo).then((data) => {
+      expect(data).to.be.null;
+      done();
+    });
+  });
+}).timeout(5000);
